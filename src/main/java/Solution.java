5049e1c766257997d01ed31ca2c584c8edf0e756
LiquidPlummer <kyle.plummer@revature.com>
2023-03-15 17:01:10 -0400

    frequency

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,11 +1,13 @@
 class Solution {
     /*
-    Frequency Array - This solution is very similar to the HashMap solution, but is a bit faster because it removes
+    Frequency Array - Optimal Solution
+
+    This solution is very similar to the HashMap solution, but is a bit faster because it removes
     some of the overhead involved with creating and maintaining those complex data structures.
 
     We create an array of length 26, enough room for each letter of the alphabet. Each index corresponds to a letter
-    from a to z. This is done by subtracting the ascii value of 'a' from the character, creating an offset where a -> 0
-    and z-> 26.
+    from a to z. This is done by subtracting the ASCII value of 'a' from the character, creating an offset where a -> 0
+    and z -> 26.
 
     We iterate through s increasing the frequency of each letter encountered. Then we iterate through t decrementing the
     frequency for each letter encountered. When we are done we iterate through the frequency array and make sure every
