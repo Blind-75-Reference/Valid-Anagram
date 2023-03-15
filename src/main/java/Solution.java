9943a499e61f7cf9a29ec8227b3c5aad15bdee76
LiquidPlummer <kyle.plummer@revature.com>
2023-03-15 16:46:21 -0400

    notes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,4 +1,10 @@
 class Solution {
+    /*
+    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all
+    the original letters exactly once.
+
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+     */
     public boolean isAnagram(String s, String t) {
         //write your solution here...
         return false;
