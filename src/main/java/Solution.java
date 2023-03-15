4090c0bf990e8b35fd3be8151ea675bc12c5dfbb
LiquidPlummer <kyle.plummer@revature.com>
2023-03-15 16:30:31 -0400

    hashing solution

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
+    /*
+    Hashmap solution
+     */
     public boolean isAnagram(String s, String t) {
-        //write your solution here...
-        return false;
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        //K = character, V = frequency
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+
+        //fill in the map
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            charFrequencyMap.put(c, charFrequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            Integer frequency = charFrequencyMap.get(c);
+
+            if(frequency == null) {
+                return false;
+            } else if(frequency == 1) {
+                charFrequencyMap.remove(c);
+            } else {
+                charFrequencyMap.replace(c, frequency - 1);
+            }
+        }
+
+
+        if(charFrequencyMap.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
