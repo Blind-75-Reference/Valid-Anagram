90548035c1da2d8921ad21867bb48226702c749e
LiquidPlummer <kyle.plummer@revature.com>
2023-03-15 16:49:17 -0400

    brute force notes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,4 +1,10 @@
 class Solution {
+    /*
+    In this solution we iterate through s checking if t contains each character. If so, we remove the first instance
+    of that character from t. If we ever find a character that is not present in t, we return false.
+
+    Once we have finished, if t is an anagram of s it will be empty.
+     */
     public boolean isAnagram(String s, String t) {
         for(int i = 0; i < s.length(); i++) {
             if(t.contains(s.substring(i, i + 1))) {
