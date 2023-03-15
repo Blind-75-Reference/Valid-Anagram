4288123d244c0ecb888415b8b5c5410b81167de2
LiquidPlummer <kyle.plummer@revature.com>
2023-03-15 15:47:28 -0400

    brute force solution and test cases

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //write your solution here...
-        return false;
+        for(int i = 0; i < s.length(); i++) {
+            if(t.contains(s.substring(i, i + 1))) {
+                t = t.replaceFirst(s.substring(i, i + 1), "");
+            } else {
+                return false;
+            }
+        }
+
+        if(t.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,2 +1,56 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestCases {
+    Solution solution = new Solution();
+
+    @Test
+    public void testCase1() {
+        String s = "debug";
+        String t = "budge";
+        Boolean expected = true;
+
+        Assertions.assertEquals(expected, solution.isAnagram(s, t));
+
+    }
+
+    @Test
+    public void testCase2() {
+        String s = "a";
+        String t = "b";
+        Boolean expected = false;
+
+        Assertions.assertEquals(expected, solution.isAnagram(s, t));
+
+    }
+
+    @Test
+    public void testCase3() {
+        String s = "aaa";
+        String t = "a";
+        Boolean expected = false;
+
+        Assertions.assertEquals(expected, solution.isAnagram(s, t));
+
+    }
+
+    @Test
+    public void testCase4() {
+        String s = "a";
+        String t = "aaa";
+        Boolean expected = false;
+
+        Assertions.assertEquals(expected, solution.isAnagram(s, t));
+
+    }
+
+    @Test
+    public void testCase5() {
+        String s = "coder";
+        String t = "credo";
+        Boolean expected = true;
+
+        Assertions.assertEquals(expected, solution.isAnagram(s, t));
+
+    }
 }
