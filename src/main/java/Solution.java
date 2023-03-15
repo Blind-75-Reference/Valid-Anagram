ee368dda51a4750854fbfce6edf56a14839aff0a
LiquidPlummer <kyle.plummer@revature.com>
2023-03-15 16:52:38 -0400

    sorting notes

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,7 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     /*
-    Sorting solution
+    Sorting
+
+    With this solution we get a character array for each string and sort them. Once sorted, if the strings are anagrams,
+    they should be the same and we simply compare them. If they aren't the same, they weren't anagrams.
+
+    We can also short circuit the problem if we get two strings with differing length. If we find that to be the case,
+    we can return false without checking anything else.
      */
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) {
